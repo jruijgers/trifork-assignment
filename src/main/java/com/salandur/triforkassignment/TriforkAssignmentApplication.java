398bdb4639681936588263fbc8f358b2b7b317e4
Jeoren Ruijgers <jeroen@salandur.nl>
2023-02-12 15:20:22 -0700

    Generate project

---
+++ src/main/java/com/salandur/triforkassignment/TriforkAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.salandur.triforkassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriforkAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriforkAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salandur/triforkassignment/TriforkAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salandur.triforkassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriforkAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
