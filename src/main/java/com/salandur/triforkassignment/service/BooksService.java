56e3e8d6ae7516b982c0d5e5f583a5098a659071
Jeroen Ruijgers <jeroen@salandur.nl>
2023-02-12 16:41:13 -0700

    Set transaction in BooksService

--- src/main/java/com/salandur/triforkassignment/service/BooksService.java
+++ src/main/java/com/salandur/triforkassignment/service/BooksService.java
@@ -4,12 +4,14 @@ import com.salandur.triforkassignment.domain.Book;
 import com.salandur.triforkassignment.repositories.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BooksService {
     @Autowired
     private BooksRepository booksRepository;
 
+    @Transactional(readOnly = true)
     public Iterable<Book> getAllBooks() {
         return booksRepository.findAll();
     }
